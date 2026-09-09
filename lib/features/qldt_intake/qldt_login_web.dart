5fee725c2003ca9ebee592c59ff671f0fdafc5db
cooc <minhdaonguyenad@gmail.com>
2026-09-09 15:25:15 +0700

    feat: add web QLDT login stub

---
+++ lib/features/qldt_intake/qldt_login_web.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+import 'qldt_models.dart';
+
+const bool supportsLiveQldtLogin = false;
+
+Future<ImportedScheduleData?> openQldtLogin(BuildContext context) async => null;
