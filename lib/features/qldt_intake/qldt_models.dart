11ce6bf3fa4fbed4bb475c44e72db550f94b4236
cooc <minhdaonguyenad@gmail.com>
2026-09-09 18:01:04 +0700

    perf: avoid repeated schedule list allocations

--- lib/features/qldt_intake/qldt_models.dart
+++ lib/features/qldt_intake/qldt_models.dart
@@ -86,11 +86,10 @@ final class ImportedScheduleData {
   final DateTime syncedAt;
   final String source;
 
-  List<ScheduleRecord> get classes =>
-      records.where((record) => !record.isExam).toList(growable: false);
+  Iterable<ScheduleRecord> get classes =>
+      records.where((record) => !record.isExam);
 
-  List<ScheduleRecord> get exams =>
-      records.where((record) => record.isExam).toList(growable: false);
+  Iterable<ScheduleRecord> get exams => records.where((record) => record.isExam);
 
   Map<String, Object?> toJson() => <String, Object?>{
     'displayName': displayName,
