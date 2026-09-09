fc845e4c574fdd506a3574021182ca38bcf96b2e
cooc <minhdaonguyenad@gmail.com>
2026-09-09 18:00:04 +0700

    perf: remove unused Riverpod root from app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:better_phenikaa_schedule/app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: BetterPhenikaaScheduleApp()));
+  runApp(const BetterPhenikaaScheduleApp());
 }
